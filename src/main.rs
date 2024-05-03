69cc6246d32f6ca831a15ddc4325e2df63c821d5
ndifix <yoshiki.linuxmint@gmail.com>
2024-05-03 22:12:22 +0900

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,19 @@
-fn main() {
+#![no_std]
+#![no_main]
+
+use uefi::prelude::*;
+use core::panic;
+use core::fmt::Write;
+
+#[panic_handler]
+fn panic(_info: &panic::PanicInfo) -> ! {
+  loop {}
+}
+
+#[entry]
+fn efi_main(_handle: Handle, mut st: SystemTable<Boot>) -> Status {
+
+  writeln!(st.stdout(), "Hello, world!").unwrap();
+
+  loop {}
 }
